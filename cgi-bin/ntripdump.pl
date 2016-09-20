aa0141fbb02bd38f0c2924387c4262ca0a78c756
Geoffrey Kirk <geoffrey_kirk@trimble.com>
2016-09-20 12:54:41 -0600

    fixed location of files for non blueshost installs and converted the tabs out

--- cgi-bin/ntripdump.pl
+++ cgi-bin/ntripdump.pl
@@ -36,7 +36,7 @@ my $total_mounts=0;
 print <<'EOF';
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 <script src="/jquery.tablesorter.min.js"></script>
-<link rel="stylesheet" type="text/css" href="/style.css"></link>
+<link rel="stylesheet" type="text/css" href="/css/style.css"></link>
 <script>
 $(document).ready(function()
 {
@@ -75,83 +75,83 @@ while (<>) {
 #   print "***+$_=\n";
    if ($skipped_header) {
        if ($_ ne "ENDSOURCETABLE" ) {
-			($type,$mount,$id,$format,$format_details,$carrier,$nav,$network,$country,$lat,$long,$nmea,$solution,$generator,$comp,$authentication,$fee,$bitrate,$misc) = split (/;/);
-		  if ($type eq "STR") {	
-		      printf("<tr>\n");
-		      $total_mounts++;
-	#        printf("   <TD>$type</TD>\n");
-		      if ($add_link) {
-			  if ($nmea == "0") {
-			     printf("   <TD><a href=\"ntrip_mount.sh?IP=$ip&PORT=$port&USER=$user&PASS=$password&MOUNT=$mount&FORMAT=$format\"> $mount</a></TD>\n");
-			  }
-			  else {
-			     printf("   <TD><a href=\"VRS_Mount_Front?IP=$ip&PORT=$port&USER=$user&PASS=$password&MOUNT=$mount&FORMAT=$format\"> $mount</a></TD>\n");
-			  }
-		      }
-		      else {
-			  printf("   <TD> $mount</TD>\n");
-		      }
-			  printf("   <TD> $id</TD>\n");
-			  printf("   <TD> $format</TD>\n");
-	#        printf("   <TD> $format_details</TD>\n");
-	#        printf("   <TD> $carrier</TD>\n");
-			  printf("   <TD> ");
+         ($type,$mount,$id,$format,$format_details,$carrier,$nav,$network,$country,$lat,$long,$nmea,$solution,$generator,$comp,$authentication,$fee,$bitrate,$misc) = split (/;/);
+        if ($type eq "STR") { 
+            printf("<tr>\n");
+            $total_mounts++;
+   #        printf("   <TD>$type</TD>\n");
+            if ($add_link) {
+           if ($nmea == "0") {
+              printf("   <TD><a href=\"ntrip_mount.sh?IP=$ip&PORT=$port&USER=$user&PASS=$password&MOUNT=$mount&FORMAT=$format\"> $mount</a></TD>\n");
+           }
+           else {
+              printf("   <TD><a href=\"VRS_Mount_Front?IP=$ip&PORT=$port&USER=$user&PASS=$password&MOUNT=$mount&FORMAT=$format\"> $mount</a></TD>\n");
+           }
+            }
+            else {
+           printf("   <TD> $mount</TD>\n");
+            }
+           printf("   <TD> $id</TD>\n");
+           printf("   <TD> $format</TD>\n");
+   #        printf("   <TD> $format_details</TD>\n");
+   #        printf("   <TD> $carrier</TD>\n");
+           printf("   <TD> ");
            if ($carrier == "0") {
-				  print "Code Only"
+              print "Code Only"
               }
            elsif ($carrier == "1") {
-				  print "L1"
+              print "L1"
               }
            elsif ($carrier == "2") {
-				  print "L1/L2"
+              print "L1/L2"
               }
            else {
               print "Unknown $carrier"
               }
-				 
-			  printf("</TD>\n");
-	
-			  printf("   <TD> $nav</TD>\n");
-			  printf("   <TD> $network</TD>\n");
-			  printf("   <TD> $country</TD>\n");
-			  printf("   <TD> $lat</TD>\n");
-			  printf("   <TD> $long</TD>\n");
-			  printf("   <TD> ");
+             
+           printf("</TD>\n");
+   
+           printf("   <TD> $nav</TD>\n");
+           printf("   <TD> $network</TD>\n");
+           printf("   <TD> $country</TD>\n");
+           printf("   <TD> $lat</TD>\n");
+           printf("   <TD> $long</TD>\n");
+           printf("   <TD> ");
 
-			  if ($nmea == "0") { print "Must Not Send NMEA"}
+           if ($nmea == "0") { print "Must Not Send NMEA"}
            elsif ($nmea == "1") { print "Must Send NMEA" }
-			  else { print "Unknown $nmea" }
+           else { print "Unknown $nmea" }
 
-			  printf("</TD>\n");
-			  printf("   <TD> ");
+           printf("</TD>\n");
+           printf("   <TD> ");
 
-			  if ($solution == "0") { print "Single base" }
+           if ($solution == "0") { print "Single base" }
            elsif ($solution == "1") {print "Network"}
-			  else {print "Unknown $solution"}
+           else {print "Unknown $solution"}
 
-			  printf("</TD>\n");
-			  printf("   <TD> $generator</TD>\n");
-#			  printf("   <TD> $comp</TD>\n");
-			  printf("   <TD> ");
+           printf("</TD>\n");
+           printf("   <TD> $generator</TD>\n");
+#          printf("   <TD> $comp</TD>\n");
+           printf("   <TD> ");
 
-			  if ($authentication == "N") {print "No"}
-			  elsif ( $authentication ==  "B") {print "Required"}
-			  elsif ( $authentication ==  "D") {print "Digest"}
-			  else {print "Unknown $authentication"}
-				
-			  printf("</TD>\n");
-			  printf("   <TD> ");
-			  if ($fee == "N") {print "No"}
-			  elsif ($fee == "Y") {print "Yes"}
-			  else {print "Unknown $fee"}
-				  
-			  printf("</TD>\n");
-	#        printf("   <TD> $bitrate</TD>\n");
-			  printf("   <TD> $misc</TD>\n");
-			  printf("</tr>\n");
-	#			print"$type;$mount;$id;$format;$format_details;$carrier;$nav;$network;$country;$lat;$long;$nmea;$solution;$generator;$comp;$authentication;$fee;$bitrate;$misc\n";
-				}
-			}
+           if ($authentication == "N") {print "No"}
+           elsif ( $authentication ==  "B") {print "Required"}
+           elsif ( $authentication ==  "D") {print "Digest"}
+           else {print "Unknown $authentication"}
+            
+           printf("</TD>\n");
+           printf("   <TD> ");
+           if ($fee == "N") {print "No"}
+           elsif ($fee == "Y") {print "Yes"}
+           else {print "Unknown $fee"}
+              
+           printf("</TD>\n");
+   #        printf("   <TD> $bitrate</TD>\n");
+           printf("   <TD> $misc</TD>\n");
+           printf("</tr>\n");
+   #        print"$type;$mount;$id;$format;$format_details;$carrier;$nav;$network;$country;$lat;$long;$nmea;$solution;$generator;$comp;$authentication;$fee;$bitrate;$misc\n";
+            }
+         }
        }
    else {
       $skipped_header = $_ eq "";
